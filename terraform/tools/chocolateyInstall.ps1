82f620d821c8c38792caa1aecc15717e0e388d21
James Toyer <jamestoyer@toyertechnologies.com>
2015-06-03 09:00:05 +0100

    Version updated

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$url = 'https://dl.bintray.com/mitchellh/terraform/terraform_0.3.6_windows_386.zip'
-$url64bit = 'https://dl.bintray.com/mitchellh/terraform/terraform_0.3.6_windows_amd64.zip'
+﻿$url = 'https://dl.bintray.com/mitchellh/terraform/terraform_0.5.0_windows_386.zip'
+$url64bit = 'https://dl.bintray.com/mitchellh/terraform/terraform_0.5.0_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage "terraform" "$url" "$unzipLocation" "$url64bit"
