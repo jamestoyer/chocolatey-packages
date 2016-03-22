2938f7cb74bccca746d7d1edc8b7742676bcb970
James Toyer <jamestoyer@toyertechnologies.com>
2016-03-22 16:21:11 +0000

    Terraform version 0.6.13

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$url = 'https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.12_windows_386.zip'
-$url64bit = 'https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.12_windows_amd64.zip'
+﻿$url = 'https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.13_windows_386.zip'
+$url64bit = 'https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.13_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage "terraform" "$url" "$unzipLocation" "$url64bit"
