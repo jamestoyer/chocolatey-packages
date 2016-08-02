7f744935bbb2fa7b54ec7c30c40215971a4becf4
James Toyer <jamestoyer@toyertechnologies.com>
2016-08-02 20:54:50 +0100

    Old terrform plugins removed

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -2,4 +2,20 @@
 $url64bit = 'https://releases.hashicorp.com/terraform/0.7.0/terraform_0.7.0_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old terraform plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\terraform\tools\terraform-*.*"
+  }
+} else {
+  if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform")) {
+    if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools")) {
+      # clean old plugins and ignore files
+      Write-Host "Removing old terraform plugins"
+      Remove-Item "$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools" -Include "terraform-*.*"
+    }
+  }
+}
+
 Install-ChocolateyZipPackage "terraform" "$url" "$unzipLocation" "$url64bit"
