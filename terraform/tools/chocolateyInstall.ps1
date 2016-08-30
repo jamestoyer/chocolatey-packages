6d5735d63d2038111cf4f7023cb40fdc80e069af
James Toyer <jamestoyer@toyertechnologies.com>
2016-08-30 15:07:53 +0100

    Terraform updated to v0.7.2

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,5 +1,7 @@
-﻿$url = 'https://releases.hashicorp.com/terraform/0.7.1/terraform_0.7.1_windows_386.zip'
-$url64bit = 'https://releases.hashicorp.com/terraform/0.7.1/terraform_0.7.1_windows_amd64.zip'
+﻿$checksum = '77d6342114fef22d3703bf3c32424b35bf70cc34d71172af2f847fb218fa2f82'
+$checksum64 = 'e52251f4c7ef255bfa42697f9d2d29fdd6cf79ce374df02119143299137a3c97'
+$url = 'https://releases.hashicorp.com/terraform/0.7.2/terraform_0.7.2_windows_386.zip'
+$url64bit = 'https://releases.hashicorp.com/terraform/0.7.2/terraform_0.7.2_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
@@ -18,4 +20,4 @@ if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
   }
 }
 
-Install-ChocolateyZipPackage "terraform" "$url" "$unzipLocation" "$url64bit"
+Install-ChocolateyZipPackage -PackageName "terraform" -Url "$url" -UnzipLocation "$unzipLocation" -Url64 "$url64bit" -ChecksumType 'sha256' -Checksum "$checksum" -Checksum64 "$checksum64"
