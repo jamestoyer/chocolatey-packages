6b84d5457da1e65a965948888b121fc92050ac73
James Toyer <jamestoyer@toyertechnologies.com>
2017-08-03 08:59:28 +0100

    Terraform version 0.10.0

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$checksum = '77491fcec1906654c1acd37902f3250115e3c62634a13f1c0c0f3d7e46124cc8'
-$checksum64 = '7710b187dcc84b696a59f196093537c377c1771a1d5239b38f87fb7a805f3760'
-$url = 'https://releases.hashicorp.com/terraform/0.10.0-rc1/terraform_0.10.0-rc1_windows_386.zip'
-$url64bit = 'https://releases.hashicorp.com/terraform/0.10.0-rc1/terraform_0.10.0-rc1_windows_amd64.zip'
+$checksum = 'a118202c72a8404d406e8250e2163dcf218dd31fe16f846ac3996854fa3c7613'
+$checksum64 = 'd9697b0153bed48af4b077c711197f8fca1163e94d5a1c067b077a02201be25f'
+$url = 'https://releases.hashicorp.com/terraform/0.10.0/terraform_0.10.0_windows_386.zip'
+$url64bit = 'https://releases.hashicorp.com/terraform/0.10.0/terraform_0.10.0_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
