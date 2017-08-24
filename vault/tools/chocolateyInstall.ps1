2788fb7ee97507ed83f5d16761b3dac3cd6edbcc
James Toyer <jamestoyer@toyertechnologies.com>
2017-08-24 09:48:43 +0100

    Vault version 0.8.1

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$checksum = '5dfb575f7d93b9a44a6ba0be9f0bb35604c7f7636303e2ebc27007b97aa44713'
-$checksum64 = 'dc9e9fda4cc89050b7a6ff9068acd548db52501be8028bd2f832ad3ef754ef9c'
-$url = 'https://releases.hashicorp.com/vault/0.8.0/vault_0.8.0_windows_386.zip'
-$url64bit = 'https://releases.hashicorp.com/vault/0.8.0/vault_0.8.0_windows_amd64.zip'
+﻿$checksum = '0a5e1ae2160e7237ebabc970b41e91fc42f5901000dcd185e8e7c74f532cc459'
+$checksum64 = '19f04a5989027245c88ced39032157240a49f186646a4497a15e3cc2a4839ebb'
+$url = 'https://releases.hashicorp.com/vault/0.8.1/vault_0.8.1_windows_386.zip'
+$url64bit = 'https://releases.hashicorp.com/vault/0.8.1/vault_0.8.1_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "vault" -Url "$url" -UnzipLocation "$unzipLocation" -Url64 "$url64bit" -ChecksumType 'sha256' -Checksum "$checksum" -Checksum64 "$checksum64"
