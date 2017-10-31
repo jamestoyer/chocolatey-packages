53c5edd759e5544425346f2df6f63e3c20f06cb8
W. van Kuipers <github@pwnd.nl>
2017-10-31 10:06:26 +0100

    Updated file & hash for 0.10.8 release

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$checksum = '9c7d40a3a30c3bd611fd8dd72b2aac4c0e83be898dc097cb2e7ed7a5721736c3'
-$checksum64 = '872fa58641020c242a579587838bb0842361e1daf0ae1f97fc3e9bab9a4d63af'
-$url = 'https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_windows_386.zip'
-$url64bit = 'https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_windows_amd64.zip'
+$checksum = '2518afa45d8b364802b5b58d504bbc76a47597990dffaad7292ac8ba60ec7189'
+$checksum64 = '6bbaf59aaf80b476ecbf765369678f5d8c296cb3a30e4d783ad560f0e4b81a76'
+$url = 'https://releases.hashicorp.com/terraform/0.10.6/terraform_0.10.8_windows_386.zip'
+$url64bit = 'https://releases.hashicorp.com/terraform/0.10.6/terraform_0.10.8_windows_amd64.zip'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
