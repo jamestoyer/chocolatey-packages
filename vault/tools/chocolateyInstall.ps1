fc2096f5bd3516d6ded13211dbe34718f07225c0
James Toyer <jamestoyer@toyertechnologies.com>
2018-04-07 09:50:44 +0100

    Vault version 0.9.6

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$checksum = '1d22b82bb2ec2378061f6b45e6280bd3915754acead7ff912644508249d40a2a'
-$checksum64 = '50a25a66836e45e890dcf197d510e0e6ff44fc6e1e41a34746e6fd184a9be6df'
-$version = '0.9.5'
+﻿$checksum = '2d1646a3f65d318f0f019ede1d78fe2af705b603802d5a4f755f4eb6e7228acd'
+$checksum64 = '5759a3355729648059584a0abc13bcac0a8d21c1c23db959c0795af1258734a4'
+$version = '0.9.6'
 $url = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_386.zip"
 $url64bit = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_amd64.zip"
 
