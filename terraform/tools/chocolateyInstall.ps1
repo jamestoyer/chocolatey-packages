2121e3d932f3c81c6810b7802264cc3ba74f3611
Colm Prunty <colmpruntysignups+github@gmail.com>
2018-08-28 11:15:07 +0100

    Terraform 0.11.8

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$checksum = '4e4b50de1143d72d348e6300d78aed596acae09714516d697bd4f0d9443aa7d3'
-$checksum64 = '5fd003ef20f7a6a85ced4ad30bf95698afd4d0bfd477541583ff014e96026d6c'
-$version = '0.11.7'
+﻿$checksum = '94ed500e27f6db531c696d087be3bd72539a31f1834f0a7d32d0cb9daf8c8e43'
+$checksum64 = 'b14dbb9b9b100ddcd516ad426f31a23016ce351d7481407bc91bceb342307826'
+$version = '0.11.8'
 $url = "https://releases.hashicorp.com/terraform/$($version)/terraform_$($version)_windows_386.zip"
 $url64bit = "https://releases.hashicorp.com/terraform/$($version)/terraform_$($version)_windows_amd64.zip"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
