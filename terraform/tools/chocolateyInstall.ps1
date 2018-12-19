1df2a4de123935dc2daf56ba6ef2431265ba1f49
Mark Brady <irmarkbrady@gmail.com>
2018-12-19 14:48:38 +0000

    Update version variable

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$checksum = 'f97dbb8a43081b67bc3d7176115adee0a578d724a6b0885c506f62ad39c9b2b0'
 $checksum64 = '87252cc67486ef2ff2b8501c0bf9e795a53585b4dc5c09a8aa876c2564f77991'
-$version = '0.11.10'
+$version = '0.11.11'
 $url = "https://releases.hashicorp.com/terraform/$($version)/terraform_$($version)_windows_386.zip"
 $url64bit = "https://releases.hashicorp.com/terraform/$($version)/terraform_$($version)_windows_amd64.zip"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
