ae2144e4a0ee0b45a822296bb3f58048088c2c0a
gaffneyd4 <derekmgaffney@gmail.com>
2019-03-08 20:37:08 -0500

    Ran update.ps1 for terraform to get version upgrade from 0.11.11 to 0.11.12

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url        = 'https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_windows_386.zip'
-$url64      = 'https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_windows_amd64.zip'
-$checksum   = 'f97dbb8a43081b67bc3d7176115adee0a578d724a6b0885c506f62ad39c9b2b0'
-$checksum64 = '87252cc67486ef2ff2b8501c0bf9e795a53585b4dc5c09a8aa876c2564f77991'
+$url        = 'https://releases.hashicorp.com/terraform/0.11.12/terraform_0.11.12_windows_386.zip'
+$url64      = 'https://releases.hashicorp.com/terraform/0.11.12/terraform_0.11.12_windows_amd64.zip'
+$checksum   = 'a600447a98b5078e29253e34a33a029ac1199f641125e7ff3f5d3645cc3a0470'
+$checksum64 = '8bcde36ba8cc415322abf68a2a47219e0a3bd001a66a7728dbfb332b65cdd269'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
