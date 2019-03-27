3135ff16bce58359d66bc74016a1b9c1d5b61f09
Tim Collins <tim@thecollins.team>
2019-03-27 12:59:48 +0000

    Refactored chocolateyInstall.ps1 to use splatting as requested by moderator

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -1,9 +1,12 @@
-﻿$checksum = '895252424cbdc23ed524f63741fd472a7a04fe508a58dead76d9f6422b465729'
-$checksum64 = 'b26de17832cb3a49d49017f5a3509e53d83547c275c85f81e045e58670b4061c'
-$version = '1.1.0'
-$url = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_386.zip"
-$url64bit = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_amd64.zip"
+﻿$packageArgs = @{
+  PackageName         = "vault"
+  Url                 = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_386.zip"
+  UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Url64               = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_amd64.zip"
+  Checksum            = '895252424cbdc23ed524f63741fd472a7a04fe508a58dead76d9f6422b465729'
+  ChecksumType        = 'sha256'
+  Checksum64          = 'b26de17832cb3a49d49017f5a3509e53d83547c275c85f81e045e58670b4061c'
+  version             = '1.1.0'  
+}
 
-$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Install-ChocolateyZipPackage -PackageName "vault" -Url "$url" -UnzipLocation "$unzipLocation" -Url64 "$url64bit" -ChecksumType 'sha256' -Checksum "$checksum" -Checksum64 "$checksum64"
+Install-ChocolateyZipPackage @packageArgs
