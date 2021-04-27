eb5a71ddbb51c48838923a7e5e0b7c6bc04b5d22
James Toyer <jamestoyer@users.noreply.github.com>
2021-04-27 18:56:15 +0100

    Terraform 0.11.15 (#196)
    * Terraform 0.11.15
    
    * Fix version number

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url        = 'https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_windows_386.zip'
-$url64      = 'https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_windows_amd64.zip'
-$checksum   = 'f2eb847761cba796f306880288083b4c68f5ae9dd86c6cff47023eecc9895f8f'
-$checksum64 = 'bfec66e2ad079a1fab6101c19617a82ef79357dc1b92ddca80901bb8d5312dc0'
+$url = 'https://releases.hashicorp.com/terraform/0.11.15/terraform_0.11.15_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/0.11.15/terraform_0.11.15_windows_amd64.zip'
+$checksum = '1e064e78d090c739fe1d5314e9aef53e10153cc739aa54eaf2748f353c60478c'
+$checksum64 = 'a5f15fb56558f2055d40da93c9bda8f90e4763922a0e9b7dd7d3f432e0222056'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
@@ -14,7 +14,8 @@ if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
     Write-Host "Removing old terraform plugins"
     Remove-Item "$env:ChocolateyInstall\lib\terraform\tools\terraform-*.*"
   }
-} else {
+}
+else {
   if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform")) {
     if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools")) {
       # clean old plugins and ignore files
