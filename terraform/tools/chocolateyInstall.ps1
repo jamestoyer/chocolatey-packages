114eec2f2d22b110e355fa68d1b6c444e9000896
Rui Lopes <rgl@ruilopes.com>
2022-10-23 18:12:06 +0100

    Terraform: Upgrade to 1.3.3

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url = 'https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_windows_386.zip'
-$url64 = 'https://releases.hashicorp.com/terraform/1.3.2/terraform_1.3.2_windows_amd64.zip'
-$checksum = '5bd1a46c1c14483984c6be7c3a6fdd2134af4d19fec548bb56d9d91e1ec7fc6f'
-$checksum64 = '6b0e47fff3392352ecc1264fd5b348fd17b2f2dff1a8dca9933e9bb033fdb498'
+$url = 'https://releases.hashicorp.com/terraform/1.3.3/terraform_1.3.3_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.3.3/terraform_1.3.3_windows_amd64.zip'
+$checksum = '59a18b1d069a0f9497c4347943ae82b9a73816ce73a97e8ea9febe442089df8a'
+$checksum64 = '291eec5320ac9e9a6b9fb446139726dc408c876a3a46a143452e20665951779a'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
