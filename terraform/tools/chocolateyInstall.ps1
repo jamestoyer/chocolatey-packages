3f298dfe94fe99894c4a040a7f00ff3b12b6feaa
David Gardiner <david@gardiner.net.au>
2023-07-27 18:50:03 +0930

    Terrform 1.5.4 (#276)

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url = 'https://releases.hashicorp.com/terraform/1.5.3/terraform_1.5.3_windows_386.zip'
-$url64 = 'https://releases.hashicorp.com/terraform/1.5.3/terraform_1.5.3_windows_amd64.zip'
-$checksum = '6665e7a9e688acb69d781fc81abf65108c93973fe1147503ceb4d9b9bf841bd6'
-$checksum64 = '9443e49ef028174a5bdd50d42b40a90fa0335453817af6c5ab2638e3debaf653'
+$url = 'https://releases.hashicorp.com/terraform/1.5.4/terraform_1.5.4_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.5.4/terraform_1.5.4_windows_amd64.zip'
+$checksum = 'f60b6fb555a116ca881c5e1d2a713c9651cec50802c6f01f5efdcfbffe151b64'
+$checksum64 = '51c95618180389c25c0e0aac1bbe76c5532f85b08f801e81a156968a90a29a40'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
