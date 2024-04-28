d92fa27b75e85bc956dfc56e0dc5e1e19ee0b924
James Toyer <jamestoyer@gmail.com>
2024-04-28 11:53:31 +0100

    Correctly structure the package

---
+++ terraform-testing/tools/chocolateyInstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# DO NOT CHANGE THESE MANUALLY. USE update.ps1
+$url = 'https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_windows_amd64.zip'
+$checksum = 'ba05760615d4dafb7e9e6c8d110035e23ce698f4f4e4b95266a61025e7a7fe91'
+$checksum64 = '33abdea6badc8716c9f2ccdd5f7988b895a92f6c658b5857c287e7b52e7ebf4a'
+
+$unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old terraform plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\terraform\tools\terraform-*.*"
+  }
+}
+else {
+  if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform")) {
+    if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools")) {
+      # clean old plugins and ignore files
+      Write-Host "Removing old terraform plugins"
+      Remove-Item "$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools" -Include "terraform-*.*"
+    }
+  }
+}
+
+$packageParams = @{
+  PackageName   = "terraform"
+  UnzipLocation = $unzipLocation
+  Url           = $url
+  Url64         = $url64
+  Checksum      = $checksum
+  Checksum64    = $checksum64
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageParams
--- terraform-testing/update.ps1
+++ terraform-testing/update.ps1
@@ -1,43 +1,127 @@
-. $PSScriptRoot\..\python3-streams\update.ps1
+#Requires -Version 5.0
+#Requires -Modules AU
+[cmdletbinding()]
+param (
+  [switch]$Force
+)
 
-if ($MyInvocation.InvocationName -ne '.') {
-  function global:au_SearchReplace {
-    @{
-      ".\README.md" = @{
-        "(?i)(install the package )\[python\d+]\((.*)python\d+" = "`$1[$($Latest.Dependency)](`$2$($Latest.Dependency)"
-        "(?i)(the package )``python\d+``( must also)"           = "`$1``$($Latest.Dependency)```$2"
-      }
+Set-StrictMode -Version Latest
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+trap {
+  Write-Host "ERROR: $_"
+    ($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$', 'ERROR: $1' | Write-Host
+    ($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$', 'ERROR EXCEPTION: $1' | Write-Host
+  throw
+}
+
+$releases_url = "https://releases.hashicorp.com/terraform"
+$releases_index_url = "$releases_url/index.json"
+
+function Get-TerraformBuild($builds, $os, $arch) {
+  foreach ($build in $builds) {
+    if (($build.os -eq $os) -and ($build.arch -eq $arch)) {
+      return $build
+    }
+  }
+  throw "Terraform build for os = $os and arch = $arch not found"
+}
+
+function Get-Shasums($url) {
+  $response = Invoke-RestMethod -Uri $url
+  Write-Verbose $response
+  $shasums = @{}
+  foreach ($line in ($response -split '\n')) {
+    if ($line) {
+      $sha, $file = $line -split '  '
+      $shasums[$file] = $sha
+    }
+  }
+  return $shasums
+}
+
+function GetStreams() {
+  param($terraform_releases)
+  $terraform_releases = Invoke-RestMethod $releases_index_url
+
+  $all_versions = @{ }
+  foreach ($release in $terraform_releases.versions.PSObject.Properties) {
+    # Only consider releases that are 1.0.0 or later
+    $version = Get-Version $release.Name
+    if ($version.Version.Major -eq 0) {
+      continue
+    }
+
+    $all_versions[$release.Name] = $release.Value
+  }
+
+  # find latest version of each supported Terraform version
+  $latest_versions = @{ }
+  $all_versions.GetEnumerator() | ForEach-Object {
+    $version = Get-Version $_.Name
+    if ($latest_versions.ContainsKey($version.Version.Minor)) {
+      $known_latest_version = Get-Version $latest_versions[$version.Version.Minor]
+    }
+    else {
+      $known_latest_version = Get-Version '0.0.0'
+    }
+    if ($version -gt $known_latest_version) {
+      $latest_versions[$version.Version.Minor] = $_.Name
+    }
+  }
+
+  $streams = @{ }
+
+  $latest_versions.GetEnumerator() | ForEach-Object {
+    $latest_version = $_.Value
+    $version = Get-Version $latest_version
+    $major_version = $version.Version.Major
+    $minor_version = $version.Version.Minor
+    $versionTwoPart = "$major_version.$minor_version"
+
+    $version_details = $all_versions[$latest_version]
+
+    $shasums = Get-Shasums "$($releases_url)/$($latest_version)/$($version_details.shasums)"
+    Write-Verbose (ConvertTo-Json $shasums)
+    $build32 = Get-TerraformBuild $version_details.builds 'windows' '386'
+    Write-Verbose (ConvertTo-Json $build32)
+    $build64 = Get-TerraformBuild $version_details.builds 'windows' 'amd64'
+    Write-Verbose (ConvertTo-Json $build64)
+
+    $streams[$versionTwoPart] = @{
+      Version      = $latest_version
+      URL32        = $build32.url
+      URL64        = $build64.url
+      Checksum32   = $shasums[$build32.filename]
+      Checksum64   = $shasums[$build64.filename]
+      ChangelogUrl = "https://github.com/hashicorp/terraform/blob/$latest_version/CHANGELOG.md"
+    }
+  }
+
+  Write-Host $streams.Count 'streams collected'
+  $streams
+}
+
+function GetReleasesStreams {
+  $terraform_releases = Invoke-RestMethod $releases_index_url
+
+  GetStreams $terraform_releases
+}
+
+function global:au_GetLatest {
+  @{ Streams = GetReleasesStreams }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]url\s*=\s*)'.*'"        = "`${1}'$($Latest.URL32)'"
+      "(?i)(^[$]url64\s*=\s*)'.*'"      = "`${1}'$($Latest.URL64)'"
+      "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
+      "(?i)(^[$]checksum64\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum64)'"
     }
   }
 }
 
-function global:au_BeforeUpdate {
-  SetCopyright
-}
-
-function global:au_AfterUpdate($Package) {
-  Set-DescriptionFromReadme $Package -SkipFirst 2
-  Update-Metadata -data @{
-    dependency = "$($Latest.Dependency)|[$($Latest.Version)]"
-    copyright  = $Latest.Copyright
-    licenseUrl = $Latest.LicenseUrl
-  }
-}
-
-function global:au_GetLatest {
-  $streams = GetReleaseFilesStreams
-  $streams.Keys | ForEach-Object {
-    $item = $streams[$_]
-    $pkgName = $item.PackageName
-    $item.Remove('PackageName') | Out-Null
-    $item.Remove('Title') | Out-Null
-    $item['Dependency'] = $pkgName
-  }
-
-
-  @{ Streams = $streams }
-}
-
-if ($MyInvocation.InvocationName -ne '.') {
-  update -ChecksumFor none -NoReadme
-}
+update -ChecksumFor none
+Update-Package -NoReadme -ChecksumFor none -Force:$Force
