225e611ae71ab190c7d779c3adc7269458a5df38
jamestoyer <jamestoyer@users.noreply.github.com>
2024-08-08 01:40:35 +0000

    Update Terraform Chocolatey packages

--- terraform/tools/chocolateyInstall.ps1
+++ terraform/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url = 'https://releases.hashicorp.com/terraform/1.10.0-alpha20240730/terraform_1.10.0-alpha20240730_windows_386.zip'
-$url64 = 'https://releases.hashicorp.com/terraform/1.10.0-alpha20240730/terraform_1.10.0-alpha20240730_windows_amd64.zip'
-$checksum = 'd78f033d978f810d3a03f276d7fac737696d61681e692f3484e5d250d7572ed8'
-$checksum64 = '2ec906c6a9d3639d813c28ae186408cac9d81063ec39716b3e0c7ef69f385877'
+$url = 'https://releases.hashicorp.com/terraform/1.9.4/terraform_1.9.4_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.9.4/terraform_1.9.4_windows_amd64.zip'
+$checksum = '0978dcff8b2cd0358881408a882f8c7ea44be5f8c3512238f3d7b6ee61454e95'
+$checksum64 = 'd9b0dcc5e7d31a290bf77f662a969d45e5ed1b760b4c2e0e2ab2770ba426d27f'
 
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
